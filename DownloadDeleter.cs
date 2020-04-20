73d156feec4adbc4414ca415a3b18ae81ad8a4be
Dalton Eschberger <eschberger.dalton@gmail.com>
2020-04-20 10:19:50 -0500

    init

---
+++ DownloadDeleter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace AutomateDownloadDeleter
+{
+	class DownloadDeleter
+	{
+		private readonly Timer _timer;
+
+		public DownloadDeleter()
+		{
+			_timer = new Timer(10000){ AutoReset = true };
+			_timer.Elapsed += timer_Elapsed;
+		}
+		private void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			string path = @"D:\CodeZone\C#\AutomateDownloadDeleter\Test";
+			DirectoryInfo directory = new DirectoryInfo(path);
+
+			foreach (FileInfo file in directory.GetFiles())
+			{
+				file.Delete();
+			}
+			foreach (DirectoryInfo dir in directory.GetDirectories())
+			{
+				dir.Delete(true);
+			}
+			Console.WriteLine("Done");
+
+		}
+		public void Start()
+		{
+			_timer.Start();
+		}
+		public void Stop()
+		{
+			_timer.Stop();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Topshelf;
+
+namespace AutomateDownloadDeleter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var exitCode = HostFactory.Run(x =>
+			{
+				x.Service<DownloadDeleter>(s =>
+				{
+					s.ConstructUsing(deleter => new DownloadDeleter());
+					s.WhenStarted(deleter => deleter.Start());
+					s.WhenStopped(deleter => deleter.Stop());
+				});
+				x.RunAsLocalSystem();
+
+				x.SetServiceName("DownloadDeleter");
+				x.SetDisplayName("Download Deleter");
+				x.SetDescription("A simple Download folder cleaner script. Deletes everything in download folder every week.");
+			});
+
+			int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+			Environment.ExitCode = exitCodeValue;
+		}
+	}
+}
